bdfdba7bf4cae1391b6cce41522e67f808a2de04
thuviengithub0208 <khoaphp@yahoo.com>
2017-09-29 19:07:22 +0700

    Initial commit

---
+++ app/src/main/java/com/ptp/phamtanphat/volleyobjectrequest0208/MainActivity.java
@@ -0,0 +1,41 @@
+package com.ptp.phamtanphat.volleyobjectrequest0208;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ReadJsonObject("https://khoapham.vn/KhoaPhamTraining/json/tien/demo1.json");
+    }
+
+    private void ReadJsonObject(String url) {
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("BBB",error.getMessage().toString());
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+}
